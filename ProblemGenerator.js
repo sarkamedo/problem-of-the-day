893c0f8563ee610204effbb5e43ba19f4f0d2a78
Sam <sarkamedo@gmail.com>
2024-09-01 13:31:28 -0400

    Update ProblemGenerator.js

--- ProblemGenerator.js
+++ ProblemGenerator.js
@@ -27,28 +27,5 @@ export const generateAProblem = async (event) => {
     
     const daysSinceStart = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000));
     const problemIndex = daysSinceStart % problemList.length;
-    return problemList[problemIndex];
-    
-    
-    const test_method = () => {
-      const startDate = new Date('2024-08-01');
-      const getNextProblemIndex = (problemList, startDate, currentDate) => {
-        const daysSinceStart = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000));
-        return daysSinceStart % problemList.length;
-      };
-      // Tests
-      const threeProblems = ['Problem 1', 'Problem 2', 'Problem 3'];
-      const fiveProblems = ['Problem 1', 'Problem 2', 'Problem 3', 'Problem 4', 'Problem 5'];
-      threeProblems.forEach((value, index) => {
-        const newDay = new Date(startDate.getTime() + (index * 24 * 60 * 60 * 1000));
-        console.log(`Three problem index: ${getNextProblemIndex(threeProblems, startDate, newDay)}`);
-      });
-      fiveProblems.forEach((value, index) => {
-        const newDay = new Date(startDate.getTime() + (index * 24 * 60 * 60 * 1000));
-        console.log(`Five problem index: ${getNextProblemIndex(fiveProblems, startDate, newDay)}`);
-      });
-      console.log(`Start with three problems: ${getNextProblemIndex(threeProblems, startDate, new Date('2024-08-03'))}`);
-      console.log(`Switch to with five problems: ${getNextProblemIndex(fiveProblems, startDate, new Date('2024-08-04'))}`);
-      }
-    
+    return problemList[problemIndex];  
   };
